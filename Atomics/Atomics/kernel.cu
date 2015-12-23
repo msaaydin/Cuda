be7bbe3114aa8a3ba2dfe5710e17a12d23638c96
Musa <maydin@fsm.edu.tr>
2015-12-23 03:00:40 +0200

    23122015

--- Atomics/Atomics/kernel.cu
+++ Atomics/Atomics/kernel.cu
@@ -19,7 +19,7 @@ void print_array(int *array, int size)
 
 __global__ void increment_naive(int *g)
 {
-	// which thread is this?
+	
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 
 	// each thread to increment consecutive elements, wrapping at ARRAY_SIZE
@@ -29,7 +29,7 @@ __global__ void increment_naive(int *g)
 
 __global__ void increment_atomic(int *g)
 {
-	// which thread is this?
+	
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 
 	// each thread to increment consecutive elements, wrapping at ARRAY_SIZE
@@ -43,11 +43,11 @@ int main(int argc, char **argv)
 	printf("%d total threads in %d blocks writing into %d array elements\n",
 		NUM_THREADS, NUM_THREADS / BLOCK_WIDTH, ARRAY_SIZE);
 
-	// declare and allocate host memory
+	
 	int h_array[ARRAY_SIZE];
 	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
 
-	// declare, allocate, and zero out GPU memory
+	
 	int * d_array;
 	cudaMalloc((void **)&d_array, ARRAY_BYTES);
 	cudaMemset((void *)d_array, 0, ARRAY_BYTES);
@@ -58,12 +58,12 @@ int main(int argc, char **argv)
 	increment_atomic <<<NUM_THREADS / BLOCK_WIDTH, BLOCK_WIDTH >>>(d_array);
 	timer.Stop();
 
-	// copy back the array of sums from GPU and print
+	
 	cudaMemcpy(h_array, d_array, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 	print_array(h_array, ARRAY_SIZE);
 	printf("Time elapsed = %g ms\n", timer.Elapsed());
 
-	// free GPU memory allocation and exit
+	
 	cudaFree(d_array);
 	return 0;
 }
